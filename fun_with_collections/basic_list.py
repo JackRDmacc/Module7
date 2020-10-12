3c58f13e34e2933c18bd81e89a507e16a29954d1
Reser, Jack C <jcreser@dmacc.edu>
2020-10-12 17:19:16 -0500

    updated make_list()

--- fun_with_collections/basic_list.py
+++ fun_with_collections/basic_list.py
@@ -3,7 +3,12 @@ This program
 10/12/20"""
 
 def make_list():
-    pass
+    my_list = []
+    #my_input = 0
+    for x in range(3):
+        #my_input = get_input()
+        my_list.insert(x, int(get_input()))
+    return my_list
 
 def get_input():
     pass
