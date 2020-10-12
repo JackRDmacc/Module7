abec5345a988144864adf2d5b0b5966306a14ed4
Reser, Jack C <jcreser@dmacc.edu>
2020-10-12 17:35:56 -0500

    updated get_input() and docstring

--- fun_with_collections/basic_list.py
+++ fun_with_collections/basic_list.py
@@ -1,14 +1,22 @@
 """Jack Reser
-This program
+This program accepts input into a list and then returns the list
 10/12/20"""
+
 
 def make_list():
     my_list = []
-    #my_input = 0
     for x in range(3):
-        #my_input = get_input()
         my_list.insert(x, int(get_input()))
     return my_list
 
+
 def get_input():
-    pass
+    x = input("Please enter a number:")
+    if not x.isdigit():
+        raise Exception("Non-numeric input")
+    else:
+        return int(x)
+
+
+if __name__ == '__main__':
+    print(get_input())
