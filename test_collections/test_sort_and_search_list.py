698c279f9cd234e396e2cac55181466ba6119a35
Reser, Jack C <jcreser@dmacc.edu>
2020-10-12 19:11:01 -0500

    added test test_sort_list(). test fails

--- test_collections/test_sort_and_search_list.py
+++ test_collections/test_sort_and_search_list.py
@@ -12,6 +12,10 @@ class MyTestCase(unittest.TestCase):
     def test_item_not_found(self, input):
         self.assertEqual(topic1.search_list(15), -1)
 
+    @patch('fun_with_collections.sort_and_search_list.make_list', return_value=[45,31,5])
+    def test_sort_list(self, input):
+        self.assertEqual(topic1.sort_list(), [5,31,45])
+
 
 if __name__ == '__main__':
     unittest.main()
